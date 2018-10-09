6741cafef346b0a646b5f0d40ccc16fe53e147e3
fcu-d0449079 <ink_zhuang@yahoo.com.tw>
2018-10-09 19:42:44 +0800

    sign

--- app/src/main/java/com/THLight/USBeacon/Sample/ui/Registered.java
+++ app/src/main/java/com/THLight/USBeacon/Sample/ui/Registered.java
@@ -4,19 +4,20 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-import com.THLight.USBeacon.Sample.ScanediBeacon;
 import com.THLight.USBeacon.Sample.R;
 
 public class Registered extends Activity {
 
-    protected void onCreat(Bundle saveInstanceState){
+    protected void onCreate(Bundle saveInstanceState){
         super.onCreate(saveInstanceState);
         setContentView(R.layout.registered);
-
-        Bundle bundle = this.getIntent().getExtras();
-        String Name = bundle.getString("Name");
+        String Name = getIntent().getStringExtra("Name");
         TextView showname = (TextView)findViewById(R.id.uName);
         showname.setText(Name);
+
+        String uuid = getIntent().getStringExtra("UUID");
+        TextView showuuid = (TextView)findViewById(R.id.UUID);
+        showuuid.setText(uuid);
     }
 
 }
